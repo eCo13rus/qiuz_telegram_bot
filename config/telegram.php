658d541a591bac3e670a3ddf8eb96b72454747c0
Илья <asd.mat@mail.ru>
2024-02-03 19:13:29 +0300

    update webhook

--- config/telegram.php
+++ config/telegram.php
@@ -34,7 +34,7 @@ return [
         'mybot' => [
             'token' => env('TELEGRAM_BOT_TOKEN', 'YOUR-BOT-TOKEN'),
             'certificate_path' => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
-            'webhook_url' => env('TELEGRAM_WEBHOOK_URL', 'https://7a77-5-75-229-140.ngrok-free.app/telegram-webhook'),
+            'webhook_url' => env('TELEGRAM_WEBHOOK_URL', 'https://ddc0-5-75-229-140.ngrok-free.app/telegram-webhook'),
             'commands' => [
                 //Acme\Project\Commands\MyTelegramBot\BotCommand::class
                 App\Telegram\Commands\StartCommand::class,
