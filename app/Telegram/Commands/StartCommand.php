f5217eaa577ed303d8b4ba1d8c304de1eb6fb196
Илья <asd.mat@mail.ru>
2024-02-04 14:03:47 +0300

    update url

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -5,11 +5,11 @@ namespace App\Telegram\Commands;
 use Illuminate\Support\Facades\Log;
 use Telegram\Bot\Actions;
 use Telegram\Bot\Commands\Command;
+use Telegram\Bot\Exceptions\TelegramResponseException; // Импортируем исключение
 
 class StartCommand extends Command
 {
     protected string $name = 'start';
-
     protected string $description = 'Стартовая команда, выводит инструкции';
 
     public function getName(): string
@@ -19,7 +19,17 @@ class StartCommand extends Command
 
     public function handle()
     {
-        $this->replyWithChatAction(['action' => Actions::TYPING]);
-        $this->replyWithMessage(['text' => 'Привет! Это квиз-игра с нашим ботом. Чтобы продолжить, выбери команду /quiz']);
+        try {
+            $this->replyWithChatAction(['action' => Actions::TYPING]);
+            $this->replyWithMessage(['text' => 'Привет! Это квиз-игра с нашим ботом. Чтобы продолжить, выбери команду /quiz']);
+        } catch (TelegramResponseException $e) {
+            if ($e->getCode() == 403) {
+                Log::error("Ошибка: бот был заблокирован пользователем. Исключение: {$e->getMessage()}");
+            } else {
+                Log::error("Произошла ошибка с Telegram API: {$e->getMessage()}");
+            }
+            // В этом месте можно добавить дополнительную логику обработки ошибки,
+            // например, удалить пользователя из списка активных чатов, если такой есть.
+        }
     }
 }
--- config/telegram.php
+++ config/telegram.php
@@ -34,7 +34,7 @@ return [
         'mybot' => [
             'token' => env('TELEGRAM_BOT_TOKEN', 'YOUR-BOT-TOKEN'),
             'certificate_path' => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
-            'webhook_url' => env('TELEGRAM_WEBHOOK_URL', 'https://ddc0-5-75-229-140.ngrok-free.app/telegram-webhook'),
+            'webhook_url' => env('TELEGRAM_WEBHOOK_URL', 'https://861f-5-161-102-73.ngrok-free.app/telegram-webhook'),
             'commands' => [
                 //Acme\Project\Commands\MyTelegramBot\BotCommand::class
                 App\Telegram\Commands\StartCommand::class,
