dccc9ca9721ad655098d4297d18abbd600ab3c22
Илья <asd.mat@mail.ru>
2024-03-01 19:27:56 +0300

    update

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -58,27 +58,39 @@ class StartCommand extends Command
         $keyboard = [];
         $answers = $question->answers->toArray();
 
-        for ($i = 0; $i < count($answers); $i += 2) {
-            $row = [];
-
-            if (isset($answers[$i])) {
-                $row[] = [
-                    'text' => $answers[$i]['text'],
-                    'callback_data' => "question_{$question->id}_answer_{$answers[$i]['id']}"
-                ];
+        // Если текущий вопрос имеет ID 66, выстраиваем кнопки вертикально
+        if ($question->id == 66) {
+            foreach ($answers as $answer) {
+                $keyboard[] = [[
+                    'text' => $answer['text'],
+                    'callback_data' => "question_{$question->id}_answer_{$answer['id']}"
+                ]];
             }
+        } else {
+            // Исходная логика для других вопросов
+            for ($i = 0; $i < count($answers); $i += 2) {
+                $row = [];
 
-            if (isset($answers[$i + 1])) {
-                $row[] = [
-                    'text' => $answers[$i + 1]['text'],
-                    'callback_data' => "question_{$question->id}_answer_{$answers[$i + 1]['id']}"
-                ];
-            }
+                if (isset($answers[$i])) {
+                    $row[] = [
+                        'text' => $answers[$i]['text'],
+                        'callback_data' => "question_{$question->id}_answer_{$answers[$i]['id']}"
+                    ];
+                }
 
-            if (!empty($row)) {
-                $keyboard[] = $row;
+                if (isset($answers[$i + 1])) {
+                    $row[] = [
+                        'text' => $answers[$i + 1]['text'],
+                        'callback_data' => "question_{$question->id}_answer_{$answers[$i + 1]['id']}"
+                    ];
+                }
+
+                if (!empty($row)) {
+                    $keyboard[] = $row;
+                }
             }
         }
+
         return $keyboard;
     }
 }
